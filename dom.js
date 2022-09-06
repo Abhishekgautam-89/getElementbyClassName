92b00e33e3b5ddda7cacb34c3b62c5fcef7c7d2f
Abhishekgautam-89 <abhishek.gautam89@outlook.com>
2022-09-06 14:53:14 +0530

    Commit2

--- dom.js
+++ dom.js
@@ -1,6 +1,16 @@
-var items = document.getElementsByClassName('list-group-item');
-console.log (items);
-items[2].style.backgroundColor='green';
-for(var i=0; i<items.length; i++){
-    items[i].style.fontWeight='bold';
+// var items = document.getElementsByClassName('list-group-item');
+// console.log (items);
+// items[2].style.backgroundColor='green';
+// for(var i=0; i<items.length; i++){
+//     items[i].style.fontWeight='bold';
+// }
+var item = document.getElementsByClassName('list-another');
+for(var i=0; i<item.length; i++){
+    item[i].style.color='red';
+}
+var li = document.getElementsByTagName('li');
+console.log(li);
+for (var i=0; i<li.length; i++){
+    li[i].style.fontWeight='900';
+    li[i].style.backgroundColor='yellow';
 }
