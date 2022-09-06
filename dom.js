f985e6657a1ca6550581755c2c1e1de7fe350dd9
Abhishekgautam-89 <abhishek.gautam89@outlook.com>
2022-09-06 23:53:46 +0530

    Commit 3

--- dom.js
+++ dom.js
@@ -14,3 +14,14 @@ for (var i=0; i<li.length; i++){
     li[i].style.fontWeight='900';
     li[i].style.backgroundColor='yellow';
 }
+var secondItem = document.querySelector('.list-group-item:nth-child(2)')
+// secondItem.style.backgroundColor='green';
+var thirdItem= document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.display='none';
+
+var scndItem = document.querySelectorAll('.list-group-item');
+// scndItem[1].style.color='green';
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for (var i=0; i<odd.length; i++){
+    odd[i].style.backgroundColor='green';
+}
